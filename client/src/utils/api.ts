b208716efbc1428db997db5444d6d236618d2217
Rupesh Agarwal <rupeshagarwal728@gmail.com>
2026-05-29 11:58:04 +0530

    fix(client): Fix localStorage token key mismatch in API utility

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -15,7 +15,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:5000',
 })
 
-const adminTokenKey = 'shecan-admin-token'
+const adminTokenKey = 'adminToken'
 
 export const getAdminToken = () => localStorage.getItem(adminTokenKey)
 
